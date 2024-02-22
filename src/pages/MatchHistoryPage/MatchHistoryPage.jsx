f12eb44327206aec4883fe4f1f7da7cb3ab823c8
amayorga7904 <alexmayorga20@gmail.com>
2024-02-21 19:12:11 -0800

    switched display of other accounts to new match page

--- src/pages/MatchHistoryPage/MatchHistoryPage.jsx
+++ src/pages/MatchHistoryPage/MatchHistoryPage.jsx
@@ -1,54 +1,9 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { getToken, getUser } from '../../utilities/users-service'
+
 
 export default function MatchHistoryPage() {
-  const [otherUsers, setOtherUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const token = getToken()
-    const fetchOtherUsers = async () => {
-      try {
-        const response = await axios.get('/api/users', {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-        setOtherUsers(response.data);
-      } catch (error) {
-        setError('Error fetching other users. Please try again later.');
-        console.error('Error fetching other users:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchOtherUsers();
-  }, []);
 
   return (
-    <>
-      <h1>Your Matches</h1>
-      {loading && <div>Loading...</div>}
-      {error && <div>Error: {error}</div>}
-      <ul>
-        {otherUsers.map(user => (
-          <li key={user._id}>
-          <div>
-            <strong>Name:</strong> {user.name}
-          </div>
-          <div>
-            <strong>Bio:</strong> {user.bio}
-          </div>
-          <div>
-            <strong>Born:</strong> {user.born}
-          </div>
-        <br />
-        </li>
-        ))}
-      </ul>
-    </>
+    <h1>Your Matches</h1>
   );
 }
--- src/pages/NewMatchPage/NewMatchPage.jsx
+++ src/pages/NewMatchPage/NewMatchPage.jsx
@@ -1,37 +1,52 @@
-import { useState } from "react";
-import { getToken, getUser } from '../../utilities/users-service'
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { getToken } from '../../utilities/users-service'
 
-const ACCOUNTS_URL = '/api/accounts/public'
 
 export default function NewMatchPage() {
-  const [accounts, setAccounts] = useState([])
+  const [otherUsers, setOtherUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const getAccounts = async () => {
-    try {
-      const token = getToken()
-      const currentUser = getUser()
-      const response = await axios.get(`${ACCOUNTS_URL}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      setAccounts(response.data)
-    } catch (error) {
-      console.log('accounts not reached', error)
-    }
-  }
+  useEffect(() => {
+    const token = getToken()
+    const fetchOtherUsers = async () => {
+      try {
+        const response = await axios.get('/api/users', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        setOtherUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching other users:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    return (
-      <>
+    fetchOtherUsers();
+  }, []);
+
+  return (
+    <>
       <h1>Discover New Chemistry</h1>
-        <ul>
-          {accounts.map((account) => (
-            <li key={account._id}>
-              {account.name}
-            </li>
-          ))}
-        </ul>
-      </>
-    );
-  }
+      {loading && <div>Loading...</div>}
+      <ul>
+        {otherUsers.map(user => (
+          <li key={user._id}>
+          <div>
+            <strong>Name:</strong> {user.name}
+          </div>
+          <div>
+            <strong>Bio:</strong> {user.bio}
+          </div>
+          <div>
+            <strong>Born:</strong> {user.born}
+          </div>
+        <br />
+        </li>
+        ))}
+      </ul>
+    </>
+  );
+}
