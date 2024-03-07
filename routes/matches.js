3ac2a71026d8f6fdf9b7e1feb6e80c171eceb9ca
amayorga7904 <alexmayorga20@gmail.com>
2024-03-06 23:20:41 -0800

    switched order of routes

--- routes/matches.js
+++ routes/matches.js
@@ -6,9 +6,9 @@ const ensureLoggedIn = require('../config/ensureLoggedIn')
 
 router.post('/', ensureLoggedIn, matchCtrl.createMatch)
 
+router.get('/new/:id', ensureLoggedIn, matchCtrl.fetchMatches)
 router.get('/:id', ensureLoggedIn, matchCtrl.showMatch)
 
-router.get('/new/:id', ensureLoggedIn, matchCtrl.fetchMatches)
 
 router.put('/:id/reject/:matchId', ensureLoggedIn, matchCtrl.rejectMatch)
 
